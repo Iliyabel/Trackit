1a41cc0a3caf6df41b5a9d9543f7315a9c3b98c3
Iliya Belyak-laptop <iliyabel2003@outlook.com>
2025-05-29 19:29:55 -0700

    Fixed sizing issues.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,20 +16,6 @@ function App() {
 
         <Route path="/" element={<LoginPage />} /> 
       </Routes>
-
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav> */}
     </>
   );
 }
