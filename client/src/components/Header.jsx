aee981318ec3ea1656390c55ad8941f959d3f65f
Iliya Belyak <iliyabel2003@outlook.com>
2025-06-10 16:39:08 -0700

    Added header module styling.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Header.module.css'; 
 
 function Header() {
     return (
-        <header className="logo-header">
-            <div>
-                <img className="logo" src={'src/assets/logoText.svg'} alt="Trackit logo" />
+        <header className={styles.logoHeader}>
+            <div className={styles.logoHeaderContainer}>
+                <img src={'src/assets/logoText.svg'} alt="Trackit logo" />
             </div>
         </header>
     );
