6b5ab614eb597a422030d81deb229310461b55e4
Jordan Lavigueure <lav.jordan@live.com>
2025-06-10 20:30:33 -0700

    Added header+footer, changed image, and added home button.

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import logo from "../assets/logo.svg";
 import styles from "./NotFoundPage.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function NotFoundPage() {
   return (
+    <>
+    <Header />
     <div className={styles.container}>
-      <img src={logo} alt="App Logo" className={styles.logo} />
+      <img src="src/assets/sadFace.png" alt="Sad face" className={styles.img} />
       <h1 className={styles.heading}>404 Error</h1>
       <p className={styles.subtext}>Page Not Found</p>
+      <button className={styles.homeButton} onClick={() => window.location.href = '/'}>
+        Go to Home
+      </button>
+
     </div>
+    <Footer />
+    </>
   );
 }
 
