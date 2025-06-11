b05755cf20fa415bb0ea7f39e2b53b3cf714c830
Jordan Lavigueure <lav.jordan@live.com>
2025-06-10 19:38:27 -0700

    Updated Header to include logout/login buttons and user initials.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styles from './Header.module.css'; 
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from './AuthProvider.jsx';
+import { getUserProfile } from '../util/ApiProvider.js';
+
 
 function Header() {
+    const navigate = useNavigate();
+    const { user, logout } = useContext(AuthContext);
+    const [initials, setInitials] = useState(null);
+
+    useEffect(() => {
+        if (user.isAuthenticated) {
+            getUserProfile(user.token)
+                .then((profile) => {
+                    if (profile) {
+                        setInitials(`${profile.firstName.charAt(0).toUpperCase()}${profile.lastName.charAt(0).toUpperCase()}`);
+                    } else {
+                        setInitials(null);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error fetching user profile:', error);
+                    setInitials(null);
+                });
+        }
+    }, [user]);
+
     return (
-        <header className={styles.logoHeader}>
-            <div className={styles.logoHeaderContainer}>
-                <img src={'src/assets/logoText.svg'} alt="Trackit logo" />
+        <header className={styles.header}>
+            <img src={'src/assets/logoText.svg'} alt="Trackit logo" />
+            <div className={styles.buttonContainer}>
+                {user ? (
+                    <>
+                        <div className={styles.roundButton}>{initials}</div>
+                        <button className={styles.logoutButton} onClick={logout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <button className={styles.loginButton} onClick={() => navigate('/login')}>Login</button>
+                    </>
+                )}
             </div>
         </header>
     );
