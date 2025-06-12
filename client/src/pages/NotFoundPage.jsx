c0a497d15baf9c67d62d0d500f81ba492671d218
Iliya Belyak <iliyabel2003@outlook.com>
2025-06-12 01:20:36 -0700

    Fixed img paths for deployment.

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./NotFoundPage.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import sadFace from "../assets/sadFace.png";
 
 function NotFoundPage() {
   return (
     <>
       <Header />
       <div className={styles.container}>
-        <img src="src/assets/sadFace.png" alt="Sad face" className={styles.img} />
+        <img src={sadFace} alt="Sad face" className={styles.img} />
         <h1 className={styles.heading}>404 Error</h1>
         <p className={styles.subtext}>Page Not Found</p>
         <button className={styles.homeButton} onClick={() => window.location.href = '/'}>
