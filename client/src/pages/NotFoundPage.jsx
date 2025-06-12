0ec23151c56ccb3f9574d1c2112cfbd2195f3255
Iliya Belyak <iliyabel2003@outlook.com>
2025-06-11 21:18:10 -0700

    Fixed indentation.

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -6,17 +6,16 @@ import Footer from "../components/Footer";
 function NotFoundPage() {
   return (
     <>
-    <Header />
-    <div className={styles.container}>
-      <img src="src/assets/sadFace.png" alt="Sad face" className={styles.img} />
-      <h1 className={styles.heading}>404 Error</h1>
-      <p className={styles.subtext}>Page Not Found</p>
-      <button className={styles.homeButton} onClick={() => window.location.href = '/'}>
-        Go to Home
-      </button>
-
-    </div>
-    <Footer />
+      <Header />
+      <div className={styles.container}>
+        <img src="src/assets/sadFace.png" alt="Sad face" className={styles.img} />
+        <h1 className={styles.heading}>404 Error</h1>
+        <p className={styles.subtext}>Page Not Found</p>
+        <button className={styles.homeButton} onClick={() => window.location.href = '/'}>
+          Go to Home
+        </button>
+      </div>
+      <Footer />
     </>
   );
 }
