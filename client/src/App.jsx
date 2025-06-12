76dbeaacc3b2111b2c836eef4091a199d13f1528
Iliya Belyak <iliyabel2003@outlook.com>
2025-06-11 21:59:52 -0700

    Added ProfilePage routing.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import AuthProvider from './components/AuthProvider';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import ProfilePage from './pages/ProfilePage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          { <Route path="/dashboard" element={<DashboardPage />} />}
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/" element={<LoginPage />} /> 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
